f38d14a7cdb052fe42af7afcf692f346f3f46ded
An Khuoch <sreykhuochan@gmail.come>
2023-03-06 22:51:49 +0700

    កែប្រែ

---
+++ src/តារាងទិនន្ន័យ/តារាងកម្រិតទឹកសមុទ្រ.ts
@@ -0,0 +1,37 @@
+import ClientConstellation, { bds } from "@constl/ipa";
+
+const កម្រិតទឹកសមុទ្រ: bds.schémaSpécificationBd = {
+    licence: "ODb1-1_0",
+    motsClefs: [],
+    tableaux: [
+        {
+            cols: [
+               {
+                idColonne: 'លេខសម្គាល់',
+                idVariable: 'OBS-KSM 001'
+               },
+               {
+                idColonne: 'រយះទទឹង',
+                idVariable: '120',
+               },
+               {
+                idColonne: 'រយះបណ្ដោយ',
+                idVariable: '200',
+               },
+               {
+                idColonne: 'ថ្ងៃខែឆ្នាំ',
+                idVariable: '2022-10-02',
+               },
+               {
+                idColonne: 'ពេលវេលា',
+                idVariable: '11:12',
+               },
+               {
+                idColonne: 'តម្លៃEc',
+                idVariable: '200',
+               }
+            ],
+            clef: "",
+        },
+    ],
+};
